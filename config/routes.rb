8e9f89ba79eb322ae21e1f9fb0ad7ede9c329829
mmolloy88 <mmolloy88@outlook.com>
2020-04-20 16:31:42 +1000

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  get 'enrollments/index'
+  get 'enrollments/new'
+  get 'enrollments/create'
+  get 'enrollments/destroy'
+  get 'courses/index'
+  get 'students/index'
+  get 'students/new'
+  get 'students/create'
+  get 'students/show'
+  get 'students/edit'
+  get 'students/update'
+  get 'students/destroy'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
